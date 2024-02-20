8891c0a217a7ebab4e00cfcd51da8c33107ebc29
ztzrk <ralif877@gmail.com>
2024-02-20 19:15:44 +0700

    feat: add color pallete

---
+++ src/shared/utils/colors.ts
@@ -0,0 +1,8 @@
+export type Colors = "primary" | "secondary" | "action" | "background_primary";
+
+export const COLORS: Record<Colors, string> = {
+    primary: "#0C1E29",
+    secondary: "#E3A678",
+    action: "#E96400",
+    background_primary: "#D9D9D9",
+};
